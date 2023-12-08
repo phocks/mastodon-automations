0c6c5bbeee072513123a073771dc2daf417c9cbc
Joshua Byrd <phocks@gmail.com>
2023-12-08 13:16:08 +1000

    Working

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use megalodon::{entities, error, generator, SNS};
+use std::env;
+
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+
+    let Ok(url) = env::var("MASTODON_URL") else {
+        println!("Specify MASTODON_URL!!");
+        return
+    };
+
+    match instance(url.as_str()).await {
+        Ok(response) => {
+            println!("{:?}", response);
+        }
+        Err(err) => {
+            println!("{:#?}", err);
+        }
+    }
+}
+
+async fn instance(url: &str) -> Result<entities::Instance, error::Error> {
+    let client = generator(SNS::Mastodon, url.to_string(), None, None);
+    let res = client.get_instance().await?;
+    Ok(res.json())
+}
